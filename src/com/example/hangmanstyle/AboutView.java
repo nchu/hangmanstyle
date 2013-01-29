a717e1ee930f97ce6b52415c56ce121846725fa1
Kristofer Selbekk <kris@kristoferselbekk.com>
2013-01-29 12:37:03 +0100

    added main view and about view

---
+++ src/com/example/hangmanstyle/AboutView.java
@@ -0,0 +1,13 @@
+package com.example.hangmanstyle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutView extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceBundle) {
+		super.onCreate(savedInstanceBundle);
+	}
+	
+}
---
+++ src/com/example/hangmanstyle/MainView.java
@@ -0,0 +1,40 @@
+package com.example.hangmanstyle;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+
+public class MainView extends Activity {
+	
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+    
+    protected void startGame(View view) {
+    	try {
+    		Intent i = new Intent(this, Game.class);
+    		startActivity(i);
+    	}
+    	catch(Exception e) {
+    		Log.e("exception", "Could not start game - intent failed: " + e.getMessage());
+    	}
+    }
+    
+    protected void showAbout(View view) {
+    	try {
+    		Intent i = new Intent(this, AboutView.class);
+    		startActivity(i);
+    	}
+    	catch(Exception e) {
+    		Log.e("exception", "Could not show about page - intent failed: " + e.getMessage());
+    	}
+    }
+
+   
+    
+}
