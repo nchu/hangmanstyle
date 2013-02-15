2ed7e419cb2704bda886710bb56f9e8d43569966
Kristofer Selbekk <kris@kristoferselbekk.com>
2013-02-15 12:27:13 +0100

    Added bg to game page

--- gen/com/example/hangmanstyle/R.java
+++ gen/com/example/hangmanstyle/R.java
@@ -16,16 +16,17 @@ public final class R {
     public static final class drawable {
         public static final int about_bg=0x7f020000;
         public static final int fuckyeah=0x7f020001;
-        public static final int hangman_style_bg=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int okay=0x7f020004;
-        public static final int p1=0x7f020005;
-        public static final int p2=0x7f020006;
-        public static final int p3=0x7f020007;
-        public static final int p4=0x7f020008;
-        public static final int p5=0x7f020009;
-        public static final int p6=0x7f02000a;
-        public static final int p7=0x7f02000b;
+        public static final int game_bg=0x7f020002;
+        public static final int hangman_style_bg=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int okay=0x7f020005;
+        public static final int p1=0x7f020006;
+        public static final int p2=0x7f020007;
+        public static final int p3=0x7f020008;
+        public static final int p4=0x7f020009;
+        public static final int p5=0x7f02000a;
+        public static final int p6=0x7f02000b;
+        public static final int p7=0x7f02000c;
     }
     public static final class id {
         public static final int a=0x7f080007;
