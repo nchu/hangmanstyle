3f0aa26cc78ec54b1f811dedd99787a823d7dfe2
hansel88 <hpn_x@hotmail.com>
2013-02-15 12:47:28 +0100

    styling av knapper done

--- src/com/example/hangmanstyle/MainView.java
+++ src/com/example/hangmanstyle/MainView.java
@@ -3,10 +3,12 @@ package com.example.hangmanstyle;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+
 
 public class MainView extends Activity {
 	
@@ -16,6 +18,7 @@ public class MainView extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main_page);
         setWinsAndLossesText();
     }
